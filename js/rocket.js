496421f1ace20c7e97f0c8329d7ed61aefbfb36d
chrismj27 <chrismj27@gmail.com>
2022-12-02 03:59:01 -0500

    Finished JS Rocket

--- js/rocket.js
+++ js/rocket.js
@@ -9,12 +9,31 @@ var changeState = function(state){
 	countdownNumber = 10; //set the countdown for 10
 	document.getElementById('countdown').innerHTML=countdownNumber; //update the page with the current count
 
+	if(state==1){
+		document.getElementById('nervous').className = 'nervous';
+		document.getElementById('cant-wait').className = 'cant-wait';
+	}
+
 	//
 	if (state==2) {
 		timer = setInterval(function() {
 			countdownNumber = countdownNumber - 1;
 			document.getElementById('countdown').innerHTML=countdownNumber;
 			
+			if (countdownNumber > 4 && countdownNumber <= 7){
+				//fart in the suit
+				document.getElementById('nervous').className = 'nervous show';
+			} else{
+				document.getElementById('nervous').className = 'nervous';
+			}
+
+			if (countdownNumber > 1 && countdownNumber <= 4){
+				//ask if it will be a problem
+				document.getElementById('cant-wait').className = 'cant-wait show';
+			} else{
+				document.getElementById('cant-wait').className = 'cant-wait';
+			}
+
 			if (countdownNumber<=0) {
 				changeState(3);
 			};
