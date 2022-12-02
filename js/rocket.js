a15d5b6a27a00174b094bfddea5ab58448dac40c
chrismj27 <chrismj27@gmail.com>
2022-12-02 02:31:36 -0500

    Make the rocket pretty

--- js/rocket.js
+++ js/rocket.js
@@ -1,11 +1,15 @@
 var timer = null;
 var countdownNumber = 10;
-var changeState = function(state){
-	document.body.className = "body-state" + state;
-	clearInterval(timer);
-	countdownNumber = 10;
-	document.getElementById('countdown').innerHTML=countdownNumber;
 
+//***Begin changeState function
+
+var changeState = function(state){
+	document.body.className = "body-state" + state; //set the value of the 'class' parameter in the BODY element of the HTML page.
+	clearInterval(timer); //reset the countdown timer
+	countdownNumber = 10; //set the countdown for 10
+	document.getElementById('countdown').innerHTML=countdownNumber; //update the page with the current count
+
+	//
 	if (state==2) {
 		timer = setInterval(function() {
 			countdownNumber = countdownNumber - 1;
